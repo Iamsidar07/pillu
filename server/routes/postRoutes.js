e234294a665dfa2cde2881484fae434e5d8d84e4
Manoj Kumar <95513071+Iamsidar07@users.noreply.github.com>
2023-03-16 00:28:46 +0530

    added profilePhoto in post schema

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -34,14 +34,15 @@ router.route("/").post(async (req, res) => {
     const { name, prompt, photo } = req.body;
     const randomId = getRandomNumber();
     const profilePhoto = `https://api.multiavatar.com/${randomId}.png`;
+    console.log({profilePhoto,randomId})
     try {
         const photoUrl = await cloudinary.uploader.upload(photo);
        
         const newPost = await Post.create({
             name,
             prompt,
+            profilePhoto:profilePhoto,
             photo: photoUrl.url,
-            profilePhoto,
         })
 
         res.status(200).json({ success: true, data: newPost })
