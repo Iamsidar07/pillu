8add1fb916d005d558e8caa52e0871e2ac957877
Manoj Kumar <95513071+Iamsidar07@users.noreply.github.com>
2023-03-16 23:39:46 +0530

    Integrate random size

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -16,14 +16,20 @@ router.route('/').get((req, res) => {
     res.status(200).json({ message: 'Hello from DALL-E!' });
 });
 
+function getRandomSize(){
+    sizes = ['1024x1024', '512x512', '256x256']
+    return sizes[Math.floor(Math.random() * 3)]
+}
+
+
 router.route('/').post(async (req, res) => {
     try {
         const { prompt } = req.body;
-
+        
         const aiResponse = await openai.createImage({
             prompt,
             n: 1,
-            size: '1024x1024',
+            size:getRandomSize(),
             response_format: 'b64_json',
         });
 
